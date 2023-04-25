1b7731e274d9c39df754d92d8eab426467bcf6c1
igor.kolyaskin <igor_kolyaskin@epam.com>
2023-04-25 12:19:26 +0400

    feat: add useContext to provide data to Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useContext } from 'react';
 import { CreateProduct } from './components/CreateProduct';
 import { ErrorMessage } from './components/ErrorMessage';
 import { Loader } from './components/Loader';
@@ -6,14 +6,16 @@ import { Modal } from './components/Modal';
 import { Product } from './components/Product';
 import { useProducts } from './hooks/products';
 import { IProduct } from './models';
+import { ModalContext } from './context/ModalContext';
 
 function App() {
 
   const { loading, error, products, addProduct } = useProducts()
-  const [modal, setModal] = useState(true)
+  // const [modal, setModal] = useState(false)
+  const { modal, open: openModal, close: closeModal } = useContext(ModalContext)
 
   const createHandler = (product: IProduct) => {
-    setModal(false)
+    closeModal()
     addProduct(product)
   }
 
@@ -23,9 +25,13 @@ function App() {
       {error && <ErrorMessage error={error} />}
       {products.map(product => <Product product={product} key={product.id} />)}
 
-      {modal && <Modal title='Create new product'>
+      {modal && <Modal title='Create new product' onClose={closeModal}>
         <CreateProduct onCreate={createHandler} />
       </Modal>}
+      <button
+        className='fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2'
+        onClick={openModal}
+      >+</button>
     </div>
   )
 }
